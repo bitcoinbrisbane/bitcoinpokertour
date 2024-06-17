23d7383791241ccaef6b0f070f29000796488d04
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-06-17 12:40:13 +1000

    chore: Update Prettier configuration to use double quotes instead of single quotes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,18 +1,17 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-import axios from "axios"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import axios from "axios";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
-
 
 export const getDate = async () => {
-  try {
-    const res = await axios.get('https://plankton-app-lht9q.ondigitalocean.app/schedule')
-    const {date} = res.data[0]
-    return date
-  } catch (error) {
-    throw new Error('Cannot Catch the date')
-  } 
-}
+	try {
+		const res = await axios.get("https://plankton-app-lht9q.ondigitalocean.app/schedule");
+		const { date } = res.data[0];
+		return date;
+	} catch (error) {
+		throw new Error("Cannot Catch the date");
+	}
+};
