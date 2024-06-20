132fb46587d917649783552fa82721a4f1478f77
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-06-20 12:36:15 +1000

    chore: Update landing page links and fix Telegram link text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,16 +11,15 @@ export default async function Home() {
 		<>
 			<div className="text-center">
 				<h2 className="text-4xl font-semibold text-center text-neutral-900 dark:text-neutral-100">Welcome to the Bitcoin Poker Tour.</h2>
-				<h3 className="text-2xl font-semibold text-center text-neutral-700 dark:text-neutral-300">Live Poker. Bitcoin buy ins.  Coming soon!</h3>
+				<h3 className="text-2xl font-semibold text-center text-neutral-700 dark:text-neutral-300">Live Poker. Bitcoin buy ins. Coming soon!</h3>
 			</div>
 			<div className="flex top-2 sm:w-full countdown text-slate-900 md:w-100 justify-center mb-3">
 				<Countdown newTarget={target} />
 			</div>
 			<div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-5 lg:text-left">
-			<Link
+				<Link
 					href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
 					rel="noopener noreferrer"
 				>
 					<h2 className="mb-3 text-2xl font-semibold">
@@ -30,9 +29,8 @@ export default async function Home() {
 				</Link>
 
 				<Link
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+					href="/buybitcoin"
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
 					rel="noopener noreferrer"
 				>
 					<h2 className="mb-3 text-2xl font-semibold">
@@ -44,16 +42,16 @@ export default async function Home() {
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
 					target="_blank"
 					rel="noopener noreferrer"
-					href={"https://t.me/+4_ll8Wiu8zQ0MTE9"}>
+					href={"https://t.me/+4_ll8Wiu8zQ0MTE9"}
+				>
 					<h2 className="mb-3 text-2xl font-semibold">
 						Telegram <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
 					</h2>
-					<p className="m-0 max-w-[30ch] text-sm opacity-50">Join Our Telegram</p>
+					<p className="m-0 max-w-[30ch] text-sm opacity-50">Join our Telegram</p>
 				</Link>
 				<Link
-					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					href="/inscriptions"
 					className="group rounded-lg border border-transparent px-4 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
 					rel="noopener noreferrer"
 				>
 					<h2 className="mb-3 text-2xl font-semibold">
@@ -63,9 +61,8 @@ export default async function Home() {
 				</Link>
 
 				<Link
-					href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					href="/schedule"
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
 					rel="noopener noreferrer"
 				>
 					<h2 className="mb-3 text-2xl font-semibold">
