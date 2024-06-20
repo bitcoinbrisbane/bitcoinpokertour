41f7c54719fc3e776a307e558503d84ba217a5fb
MANUEL RIOS <m.rios56@hotmail.com>
2024-06-20 19:04:16 -0400

    Update input placeholder

--- src/components/ui/Forms/Form.tsx
+++ src/components/ui/Forms/Form.tsx
@@ -48,7 +48,7 @@ const Forms = (id: any) => {
                         <Field
                             className="mb-5 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             name='bitcoin_address'
-                            placeholder="tb1qugnsszut6dm6ggj8ut45tg83tklfcsqwv4l39q"
+                            placeholder="BTC receive address"
                             required
                         />
                         <ErrorMessage component='a' className="" name='bitcoin_address'/>
