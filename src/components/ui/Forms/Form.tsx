1de77f44c287d8dee903ae965494e09e74bbfd75
MANUEL RIOS <m.rios56@hotmail.com>
2024-06-20 21:41:04 +0000

    fix form layout

--- src/components/ui/Forms/Form.tsx
+++ src/components/ui/Forms/Form.tsx
@@ -15,7 +15,7 @@ const Forms = (id: any) => {
 
     return (
         <>
-            <div className="sm:w-full md:w-2/4  shadow-lg border rounded-lg p-6">
+            <div className="sm:w-full md:w-2/4 p-6">
                 <Formik
                     initialValues={initVals}
                     onSubmit={(
@@ -29,40 +29,30 @@ const Forms = (id: any) => {
                       }}
                 >
                     <Form className="max-w-sm mx-auto space-y-5">
-                        <label className="py-1" aria-label="disabled input mt-2" htmlFor='Name'>
-                            Name or bns:
-                        </label>
-                        <ErrorMessage component='a' className="" name='name' />
                         <Field
-                            className="bg-gray-50 border border-gray-300 text-gray-900 mb-3 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            className="mb-5 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             name='name'
-                            placeholder="John Doe"
+                            placeholder="Name or BNS"
                             required
                         />
-                        <label className="py-1" htmlFor='Email'>
-                            Email:
-                        </label>
-                        <ErrorMessage component='a' className="mb-2 text-red-400" name='email' />
+                        <ErrorMessage component='a' className="" name='name' />
                         <Field 
                             className="bg-gray-50 border border-gray-300 mb-3 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             id='email'
                             name='email'
-                            placeholder="john@doe.com"
+                            placeholder="Email"
                             validate={validateEmail}
                             required
                         />
-                        
-                        <label className="py-1 mt-2" aria-label="disabled input" htmlFor='bitcoin_address'>
-                            Bitcoin Address:
-                        </label>
-                        <ErrorMessage component='a' className="" name='bitcoin_address' />
+                        <ErrorMessage component='a' className="mb-2 text-red-400" name='email' />
                         <Field
-                            className="bg-gray-50 border border-gray-300 mb-3 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            className="mb-5 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             name='bitcoin_address'
                             placeholder="tb1qugnsszut6dm6ggj8ut45tg83tklfcsqwv4l39q"
                             required
                         />
-                        <button type="submit"  className="shadow-md hover:cursor-pointer hover:bg-blue-500 hover:text-white font-medium rounded-lg border text-sm px-5 py-2.5 text-center">Register</button>
+                        <ErrorMessage component='a' className="" name='bitcoin_address'/>
+                        <button type="submit"  className="shadow-md w-full font-bold hover:cursor-pointer hover:bg-blue-500 hover:text-white font-medium rounded-lg border text-sm px-5 py-2.5 text-center">Register</button>
                     </Form>
 
                 </Formik>
