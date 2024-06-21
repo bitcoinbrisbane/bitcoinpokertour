b1cd6931b2897ff3819b417589e005790406c2da
MANUEL RIOS <m.rios56@hotmail.com>
2024-06-21 01:46:52 +0000

    fixing Logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,13 +20,13 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={inter.className}>
 				<main className="flex h-auto sm:w-screen md:w-auto flex-col items-center justify-between p-16">
-					<div className="flex items-center">
+					<div className="flex items-center ">
 						<Image
 							className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
 							src="/btcpt.svg"
 							alt="Bitcoin Poker Tour"
-							width={360}
-							height={54}
+							width={500}
+							height={204}
 							priority
 						/>
 					</div>
