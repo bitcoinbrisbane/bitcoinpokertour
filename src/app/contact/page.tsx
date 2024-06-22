2f088c6f33eca627034b61557065a2b44ddfd3b7
dev_dubem-at-047308373210 <dubemobiwon@gmail.com>
2024-06-22 16:37:39 +0100

    fix: added email to contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,8 +2,11 @@ const Contact = () => {
 	return (
 		<main className="flex max-h-screen w-full flex-col items-center justify-between">
 			<h1 className="text-4xl font-semibold text-center text-neutral-900 dark:text-neutral-100">Contact</h1>
+			<p className="font-semibold pt-6 pb-3 text-lg">
+				Email: <span className="font-normal">admin@bitcoinpokertour.com</span>
+			</p>
 		</main>
-	)
+	);
 };
 
 export default Contact;
