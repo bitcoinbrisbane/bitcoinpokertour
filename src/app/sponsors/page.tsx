d1e8d2661a0e708d7f0964ba37000e5b310aef1b
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-06-24 08:29:19 +1000

    chore: Add sponsor information to Sponsors page

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -2,6 +2,9 @@ const Sponsors = () => {
 	return (
 		<main className="flex max-h-screen w-full flex-col items-center justify-between">
 			<h1 className="text-4xl font-semibold text-center text-neutral-900 dark:text-neutral-100">Sponsors</h1>
+
+			<p>Own node, own infrastructure</p>
+			<p>In the spirit of bitcoin, we host our own node at node.bitcoinpokertour.com</p>
 		</main>
 	);
 };
