22250976411ae6f4dbed4096f216fbd4b3329228
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-06-25 11:54:19 +1000

    refactor: Remove redundant code in schedule route

--- src/api/routes/schedule.js
+++ src/api/routes/schedule.js
@@ -12,25 +12,6 @@ const Result = require("../schemas/result");
 
 router.get("/", async (req, res) => {
   const events = await Event.find();
-
-  if (events.length === 0) {
-    const events = [
-      {
-        id: 1,
-        title: "The 'Mainnet' Test Event",
-        description:
-          "This is the first event of the Bitcoin Poker tour.  Real Bitcoin is used for this event.",
-        date: "2025-01-01 11:00:00",
-        location: "Eatons Hill, Brisbane",
-        start_stack: 20000,
-        blind_levels: 20,
-        game_type: "No Limit Texas Holdem",
-      },
-    ];
-
-    return res.json(events);
-  }
-
   res.json(events);
 });
 
