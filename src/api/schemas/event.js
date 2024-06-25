4d9bf77843640d0387b8a24857e060ef8019f67e
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-06-25 14:12:45 +1000

    feat: Add fee field to event schema and registration endpoint

--- src/api/schemas/event.js
+++ src/api/schemas/event.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const eventSchema = new mongoose.Schema({
+  // _id: mongoose.Schema.Types.ObjectId,
   title: {
     type: String,
     required: true,
@@ -26,6 +27,10 @@ const eventSchema = new mongoose.Schema({
     required: true,
   },
   buy_in: {
+    type: Number,
+    required: true,
+  },
+  fee: {
     type: Number,
     required: true,
   },
