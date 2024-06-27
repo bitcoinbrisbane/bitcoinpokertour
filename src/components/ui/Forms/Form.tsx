51404f1b96747863f8e2957833456fb868014663
Lucas Cullen <bitcoinbrisbane@users.noreply.github.com>
2024-06-27 15:12:00 +1000

    Update src/components/ui/Forms/Form.tsx

--- src/components/ui/Forms/Form.tsx
+++ src/components/ui/Forms/Form.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import { Formik, Field, Form, ErrorMessage, FormikHelpers } from "formik";
 import { postRegistration } from "@/lib/utils";
 import { IRegisterEvent } from "@/types";
