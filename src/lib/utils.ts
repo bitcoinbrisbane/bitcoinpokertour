01b16ee1bc985f78e08e9dceb6a904b2fc3d3d5d
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-06-28 15:07:16 +1000

    feat: remove hard coded urls

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,13 +3,15 @@ import { twMerge } from "tailwind-merge";
 import { IRegisterEvent } from "@/types";
 import axios from "axios";
 
+const API = "http://localhost:5001"; // process.env.API || "https://api.bitcoinpokertour.com";
+
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
 export const getDate = async () => {
 	try {
-		const { data } = await axios.get("https://api.bitcoinpokertour.com/schedule");
+		const { data } = await axios.get(`${API}/schedule`);
 		return data[0].date;
 	} catch (error) {
 		throw new Error("Failed to fetch the date from the API. Please check the network connection and the URL.");
@@ -18,7 +20,7 @@ export const getDate = async () => {
 
 export const getEvents = async () => {
 	try {
-		const { data } = await axios.get("https://api.bitcoinpokertour.com/schedule");
+		const { data } = await axios.get(`${API}/schedule`);
 		return data;
 	} catch (error) {
 		throw new Error("Failed to fetch event data from the API. Please check the network connection and the URL.");
@@ -27,7 +29,7 @@ export const getEvents = async () => {
 
 export const getEventById = async (id: string) => {
 	try {
-		const { data } = await axios.get(`https://api.bitcoinpokertour.com/schedule/${id}`);
+		const { data } = await axios.get(`${API}/schedule/${id}`);
 		return data;
 	} catch (error) {
 		throw new Error("Failed to fetch the event data. Please check the event ID, network connection, and the URL.");
@@ -43,7 +45,7 @@ export const postRegistration = async (register: IRegisterEvent) => {
 	};
 	const { id }: any = evt_id;
 	try {
-		const res = await axios.post(`https://api.bitcoinpokertour.com/registration/${id}`, registration);
+		const res = await axios.post(`${API}/registration/${id}`, registration);
 		console.log(res);
 	} catch (error) {
 		throw new Error("Failed to sent the event registrations. Please check the event ID, network connection, and the URL.");
@@ -52,7 +54,7 @@ export const postRegistration = async (register: IRegisterEvent) => {
 
 export const getRegistrations = async (id: string) => {
 	try {
-		const { data } = await axios.get(`https://api.bitcoinpokertour.com/registration/${id}`);
+		const { data } = await axios.get(`${API}/registration/${id}`);
 		return data;
 	} catch (error) {
 		throw new Error("Failed to fetch the event registrations. Please check the event ID, network connection, and the URL.");
