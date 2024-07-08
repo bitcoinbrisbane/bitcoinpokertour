bc286a38f0d425766eb7ea46da90edaa5a3474a1
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-07-08 15:01:08 +1000

    feat: Update registration address path in getRegistrationAddress function

--- src/api/utils.js
+++ src/api/utils.js
@@ -5,6 +5,9 @@ const ecc = require("tiny-secp256k1");
 const bip32 = BIP32Factory(ecc);
 const bip39 = require("bip39");
 const bitcoin = require("bitcoinjs-lib");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const ECPair = ECPairFactory(ecc);
 const network = bitcoin.networks.mainnet;
@@ -17,15 +20,20 @@ const getRegistrationAddress = (event_id, user_id) => {
 	const coin = network === bitcoin.networks.testnet ? "1" : "0";
 	const network_id = network === bitcoin.networks.testnet ? "44" : "84";
 	//m/84'/0'/0'/0/0
-	const path = `m/${network_id}'/${coin}'/${event_id}/${user_id}`;
+	const path = `m/${network_id}'/${coin}'/0'/${event_id}/${user_id}`;
+	console.log("path", path);
 	return getAddress(path);
 };
 
 const getAddress = path => {
 	const mnemonic = getMnemonic();
+	if (!bip39.validateMnemonic(mnemonic)) {
+		throw new Error("Invalid Mnemonic");
+	}
+
 	const seed = bip39.mnemonicToSeedSync(mnemonic);
 	const root = bip32.fromSeed(seed, network);
-	console.log("root", root);
+	// console.log("root", root);
 	const child = root.derivePath(path);
 	const address = bitcoin.payments.p2wpkh({
 		pubkey: child.publicKey,
@@ -38,3 +46,6 @@ module.exports = {
 	getRegistrationAddress,
 	getAddress
 };
+
+const x = getRegistrationAddress(0, 0);
+console.log("address:: ", x);
