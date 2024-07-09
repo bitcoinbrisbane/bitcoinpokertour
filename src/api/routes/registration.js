91612d1aa410811b49ca4a85d4d9f80ed8323aca
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-07-10 08:44:55 +1000

    Update BTC Pay Server credentials in .env.example

--- src/api/routes/registration.js
+++ src/api/routes/registration.js
@@ -55,9 +55,7 @@ router.post("/:eventid", async (req, res) => {
 	const count = await Registration.find({ event_id: eventid }).countDocuments();
 
 	if (process.env.BTC_PAY_SERVER) {
-
-		// const basic_auth = Buffer.from(`admin@bitcoinpokertour.com:${process.env.BTC_PAY_SERVER_PASS}`).toString("base64");
-		const basic_auth = Buffer.from(`admin@bitcoinpokertour.com:duftu5-summok-xehgaC`).toString("base64");
+		const basic_auth = Buffer.from(`${prcoess.env.BTC_PAY_SERVER_EMAIL}:${process.env.BTC_PAY_SERVER_PASSWORD}`).toString("base64");
 
 		const config = {
 			headers: {
