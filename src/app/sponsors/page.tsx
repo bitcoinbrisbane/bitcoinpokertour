495159fa2bf0968abf2e2c300de93f8ef92142be
MANUEL RIOS <m.rios56@hotmail.com>
2024-07-17 02:07:49 -0400

    Adding container

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -1,6 +1,6 @@
 const Sponsors = () => {
 	return (
-		<main className="flex max-h-screen w-full flex-col items-center justify-between">
+		<main className="flex max-h-screen w-full flex-col items-center justify-between container">
 			<h1 className="text-4xl font-semibold text-center text-neutral-900 dark:text-neutral-100">Sponsors</h1>	
 			<p className="mt-10 text-center">
 				Join us as a sponsor of the Bitcoin Poker Tour and support the launch of an innovative event that highlights the real-world applications of the Bitcoin Network. As a crypto-related business, you will engage with a community of like-minded enthusiasts and poker fans. Our focus on security, transparency, simplicity, speed, and efficiency ensures a valuable partnership. Register today to help shape the future of poker and cryptocurrency. Be a foundational partner with the Bitcoin Poker Tour.
