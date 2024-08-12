1fa44131a7d66ea029091e1d9af1044765dc4c93
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-08-12 14:33:18 +1000

    chore: add jwt auth

--- src/api/routes/players.js
+++ src/api/routes/players.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 // use bcrypt
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // use json
 router.use(express.json());
@@ -20,6 +21,42 @@ router.get("/", async (req, res) => {
 	const players = await Player.find();
 
 	res.json(players);
+});
+
+router.get("/:id", async (req, res) => {
+	const jwt = req.headers["authorization"];
+
+	if (!jwt) {
+		return res.status(401).json({ error: "Unauthorized" });
+	}
+
+	// Remove Bearer from token
+	const token = jwt.slice(7);
+	const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+	// Check if token is valid
+	if (!decoded) {
+		return res.status(401).json({ error: "Unauthorized" });
+	}
+
+	const { id } = req.params;
+
+	if (!id) {
+		return res.status(400).json({ error: "Missing required fields" });
+	}
+
+	// compare the id from the token with the id from the request
+	if (decoded.id !== id) {
+		return res.status(401).json({ error: "Unauthorized" });
+	}
+
+	const player = await Player.findById(id);
+
+	if (!player) {
+		return res.status(404).json({ error: "Player not found" });
+	}
+
+	res.json(player);
 });
 
 router.post("/", async (req, res) => {
@@ -51,6 +88,41 @@ router.post("/", async (req, res) => {
 	await player.save();
 
 	res.status(201).json(player);
+});
+
+router.post("/login", async (req, res) => {
+	const { email, password } = req.body;
+
+	if (!email || !password) {
+		return res.status(400).json({ error: "Missing required fields" });
+	}
+
+	const player = await Player.findOne({ email });
+
+	if (!player) {
+		return res.status(404).json({ error: "Player not found" });
+	}
+
+	if (!bcrypt.compareSync(password, player.password)) {
+		return res.status(401).json({ error: "Invalid password" });
+	}
+
+	// Create JWT with jsonwebtoken
+	const token = jwt.sign({ id: player._id }, process.env.JWT_SECRET, {
+		expiresIn: "24h"
+	});
+
+	const response = {
+		token,
+		player : {
+			id: player._id,
+			name: player.name,
+			email: player.email,
+			bitcoin_address: player.bitcoin_address
+		}
+	};
+
+	res.json(response);
 });
 
 
