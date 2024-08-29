cb5a77c1b542fab1730b120d6c116de221d0bf4d
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-08-29 11:22:55 +1000

    fixes #104

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,5 +1,5 @@
 import { Table, TableHeader } from "@/components/ui/table";
-import { getEvents } from "@/lib/utils";
+import { getEvents, getPastEvents } from "@/lib/utils";
 import TableRows from "@/components/ui/TableRows";
 import TableSchedule from "@/components/ui/TableSchedule";
 import { IEvents } from "@/types";
@@ -7,23 +7,49 @@ import { unstable_noStore } from "next/cache";
 
 export default async function Page() {
 	unstable_noStore();
-	const data = await getEvents();
+
+	const [events, pastEvents] = await Promise.all([getEvents(), getPastEvents()]);
 
 	return (
 		<main className="flex max-h-screen max-w-screen flex-col items-center justify-between">
 			<h1 className="text-4xl font-semibold text-center text-neutral-900 dark:text-neutral-100 mb-4">
-				{data && data.length > 0 ? "Upcoming events" : "No scheduled events"}
+				{events && events.length > 0 ? "Upcoming events" : "No scheduled events"}
 			</h1>
 
-			{data && data.length === 0 && <NoRegistrations />}
+			{events && events.length === 0 && <NoRegistrations />}
 
-			{data && data.length > 0 && (
+			{events && events.length > 0 && (
 				<Table className="">
 					<TableHeader>
 						<TableSchedule />
 					</TableHeader>
-					{data &&
-						data.map((items: IEvents) => (
+					{events &&
+						events.map((items: IEvents) => (
+							<TableRows
+								key={items.title}
+								_id={items._id}
+								date={items.date}
+								title={items.title}
+								description={items.description}
+								location={items.location}
+								buy_in={items.buy_in}
+								start_stack={items.start_stack}
+								game_type={items.game_type}
+								blind_levels={items.blind_levels}
+							/>
+						))}
+				</Table>
+			)}
+
+			<h2 className="text-2xl font-semibold text-center text-neutral-900 dark:text-neutral-100 mt-4">Previous events</h2>
+
+			{pastEvents && pastEvents.length > 0 && (
+				<Table className="">
+					<TableHeader>
+						<TableSchedule />
+					</TableHeader>
+					{pastEvents &&
+						pastEvents.map((items: IEvents) => (
 							<TableRows
 								key={items.title}
 								_id={items._id}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -34,6 +34,16 @@ export const getEvents = async () => {
 	}
 };
 
+export const getPastEvents = async (max: number = 10) => {
+	try {
+		const { data } = await axios.get(`${API}/schedule/past?max=${max}`);
+		return data;
+	} catch (error) {
+		//throw new Error("Failed to fetch event data from the API. Please check the network connection and the URL.");
+		console.error(error);
+	}
+};
+
 export const getEventById = async (id: string) => {
 	try {
 		const { data } = await axios.get(`${API}/schedule/${id}`);
