59428a9ca7077f1a82a051d92c45569c55dff5b0
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-08-29 11:03:49 +1000

    chore: dont show table if no events are scheduled

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -11,30 +11,35 @@ export default async function Page() {
 
 	return (
 		<main className="flex max-h-screen max-w-screen flex-col items-center justify-between">
-			<h1 className="text-4xl font-semibold text-center text-neutral-900 dark:text-neutral-100 mb-4">Upcoming events</h1>
-			<Table className="">
-				<TableHeader>
-					<TableSchedule />
-				</TableHeader>
-				{data ? (
-					data.map((items: IEvents) => (
-						<TableRows
-							key={items.title}
-							_id={items._id}
-							date={items.date}
-							title={items.title}
-							description={items.description}
-							location={items.location}
-							buy_in={items.buy_in}
-							start_stack={items.start_stack}
-							game_type={items.game_type}
-							blind_levels={items.blind_levels}
-						/>
-					))
-				) : (
-					<NoRegistrations />
-				)}
-			</Table>
+			<h1 className="text-4xl font-semibold text-center text-neutral-900 dark:text-neutral-100 mb-4">
+				{data && data.length > 0 ? "Upcoming events" : "No scheduled events"}
+			</h1>
+
+			{data && data.length > 0 && (
+				<Table className="">
+					<TableHeader>
+						<TableSchedule />
+					</TableHeader>
+					{data ? (
+						data.map((items: IEvents) => (
+							<TableRows
+								key={items.title}
+								_id={items._id}
+								date={items.date}
+								title={items.title}
+								description={items.description}
+								location={items.location}
+								buy_in={items.buy_in}
+								start_stack={items.start_stack}
+								game_type={items.game_type}
+								blind_levels={items.blind_levels}
+							/>
+						))
+					) : (
+						<NoRegistrations />
+					)}
+				</Table>
+			)}
 		</main>
 	);
 }
